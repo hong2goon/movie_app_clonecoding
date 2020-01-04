a0e7b2b95995c843f8f78db045c549c5bd81fbb8
hong2goon <hong2@hong2ui-MacBookAir.local>
2020-01-04 22:36:19 +0900

    Updates

---
+++ precache-manifest.87e07dde156046f2290bde229a2520c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7657d2ab08724ca8cfb6ced0d53ed994",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "42e5d6bc328cc1809677",
+    "url": "/movie_app/static/css/main.300aa444.chunk.css"
+  },
+  {
+    "revision": "ef57acdb8116d3cc26d4",
+    "url": "/movie_app/static/js/2.49ed7f4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.49ed7f4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42e5d6bc328cc1809677",
+    "url": "/movie_app/static/js/main.0e46d7fc.chunk.js"
+  },
+  {
+    "revision": "c14eec7ed0651debe84c",
+    "url": "/movie_app/static/js/runtime-main.b9d7b04d.js"
+  }
+]);
